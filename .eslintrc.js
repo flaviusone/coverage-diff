ac88c58a626b451ae03df0c5579da49261078187
Flavius Tirnacop <flaviusone@gmail.com>
2018-04-02 00:44:55 +0300

    Add prettier and linting.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['eslint:recommended', 'prettier'],
+  env: {
+    commonjs: true,
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017
+  }
+};
