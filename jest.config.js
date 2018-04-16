f8b3545d7a13adb1f3c10a82c52bbe1b153725f9
Flavius Tirnacop <flaviusone@gmail.com>
2018-04-17 00:14:23 +0300

    Add coverage script.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   clearMocks: true,
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/*.d.ts'],
+  coverageReporters: ['lcov'],
+  coverageDirectory: '<rootDir>/coverage',
   transform: {
     '^.+\\.(js|ts)$': 'ts-jest'
   },
