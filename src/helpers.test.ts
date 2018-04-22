482d2125d5207222e5fb2a0bffb8173bbb4a3262
Flavius Tirnacop <flaviusone@gmail.com>
2018-04-22 23:36:55 +0300

    Add tests for helpers.

---
+++ src/helpers.test.ts
@@ -0,0 +1,28 @@
+import { objectToMap, mapToObject } from './helpers';
+
+const myObject = {
+  foo: 'bar'
+};
+
+const myMap = new Map([['foo', 'bar'], ['bar', 'foo']]);
+
+describe('helpers', () => {
+  describe('objectToMap', () => {
+    it('should return a map', () => {
+      expect(objectToMap(myObject)).toBeInstanceOf(Map);
+    });
+
+    it('should correctly map object data', () => {
+      expect(objectToMap(myObject).get('foo')).toEqual('bar');
+    });
+  });
+
+  describe('objectToMap', () => {
+    it('should correctly create object from map', () => {
+      expect(mapToObject(myMap)).toMatchObject({
+        foo: 'bar',
+        bar: 'foo'
+      });
+    });
+  });
+});
