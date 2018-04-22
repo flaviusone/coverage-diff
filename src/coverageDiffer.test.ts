e2a1768680dde412a2f25baaf49b8fae5e105112
Flavius Tirnacop <flaviusone@gmail.com>
2018-04-23 01:07:27 +0300

    Move fixtures into separate file.

--- src/coverageDiffer.test.ts
+++ src/coverageDiffer.test.ts
@@ -1,99 +1,10 @@
-import { IJsonSummary } from './common';
 import { coverageDiffer } from './coverageDiffer';
-
-const fileNotCovered: IJsonSummary = {
-  fileA: {
-    lines: {
-      total: 2,
-      covered: 0,
-      skipped: 0,
-      pct: 0
-    },
-    statements: {
-      total: 2,
-      covered: 0,
-      skipped: 0,
-      pct: 0
-    },
-    functions: {
-      total: 2,
-      covered: 0,
-      skipped: 0,
-      pct: 0
-    },
-    branches: {
-      total: 2,
-      covered: 0,
-      skipped: 0,
-      pct: 0
-    }
-  }
-};
-
-const fileHalfCovered: IJsonSummary = {
-  fileA: {
-    lines: {
-      total: 2,
-      covered: 1,
-      skipped: 0,
-      pct: 50
-    },
-    statements: {
-      total: 2,
-      covered: 1,
-      skipped: 0,
-      pct: 50
-    },
-    functions: {
-      total: 2,
-      covered: 1,
-      skipped: 0,
-      pct: 50
-    },
-    branches: {
-      total: 2,
-      covered: 1,
-      skipped: 0,
-      pct: 50
-    }
-  }
-};
-
-const fileFullCovered: IJsonSummary = {
-  fileA: {
-    lines: {
-      total: 2,
-      covered: 2,
-      skipped: 0,
-      pct: 100
-    },
-    statements: {
-      total: 2,
-      covered: 2,
-      skipped: 0,
-      pct: 100
-    },
-    functions: {
-      total: 2,
-      covered: 2,
-      skipped: 0,
-      pct: 100
-    },
-    branches: {
-      total: 2,
-      covered: 2,
-      skipped: 0,
-      pct: 100
-    }
-  }
-};
-
-const newFile: IJsonSummary = {
-  ...fileFullCovered,
-  fileB: {
-    ...fileFullCovered.fileA
-  }
-};
+import {
+  fileFullCovered,
+  fileHalfCovered,
+  fileNotCovered,
+  newFile
+} from './fixtures.test';
 
 describe('coverageDiffer', () => {
   describe('file coverage increased ', () => {
